f6b2ec93d0a8d95cb6dc8c445861082dabf1d582
Mauris Alonso <mauris.alonso@gmail.com>
2016-07-20 22:23:26 -0400

    Created partials, basic html, and PagesController

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PageController@getIndex');
+Route::get('about', 'PageController@getAbout');
+Route::get('contact', 'PageController@getContact');
---
+++ resources/views/partials/_footer.blade.php
@@ -0,0 +1,3 @@
+<hr>
+
+<p class="text-center">Copyright 2016 - All Rights Reserved</p>
