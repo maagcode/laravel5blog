c74ceda8b2d6e7bfb3e6fb28f8c340947e9973a7
Mauris Alonso <mauris.alonso@gmail.com>
2016-07-21 22:53:00 -0400

    Installed Laravel Collective Forms, posts.created made

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -4,8 +4,23 @@
 
 @section('content')
 
-    <p>
-        Form placeholder
-    </p>
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Create New Post</h1>
+
+            {!! Form::open(array('route' => 'posts.store')) !!}
+
+                {{ Form::label('title', 'Title:') }}
+                {{ Form::text('title', null, ['class' => 'form-control', 'required' => '', 'maxlength' => '255']) }}
+
+                {{ Form::label('body', 'Post Body:') }}
+                {{ Form::textarea('body', null, ['class' => 'form-control']) }}
+
+                {{ Form::submit('Create Post', ['class' => 'btn btn-success btn-lg btn-block', 'required' => '', 'style' => 'margin-top:20px;']) }}
+
+
+            {!! Form::close() !!}
+        </div>
+    </div>
 
 @stop
