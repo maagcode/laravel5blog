92debdaaccbaebecfb3103729267b40dc9b43f6f
Mauris Alonso <mauris.alonso@gmail.com>
2016-07-26 13:58:35 -0400

    Auth - Login/Register added

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,11 +11,23 @@
 |
 */
 
+// Authentication routes
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+// Registration routes
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
+
+// Static pages routes
 Route::get('/', 'PageController@index');
 Route::get('about', 'PageController@about');
 Route::get('contact', 'PageController@contact');
 
+// Blog routes
 Route::get('blog/{slug}', ['as' => 'blog.single', 'uses' => 'BlogController@single'])->where('slug', '[\w\d\-\_]+');
 Route::get('blog', ['uses' => 'BlogController@blogIndex', 'as' => 'blog.index']);
 
+// Resources routes
 Route::resource('posts', 'PostController');
